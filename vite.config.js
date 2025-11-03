d751f733b5ada06749ea7a6a6c03443a19b1d771
Hasitha-Kaluarachchi <kaluarachchigehasitha@gmail.com>
2025-11-03 21:00:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
-  base: "/my-social-feed/",
-};
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
